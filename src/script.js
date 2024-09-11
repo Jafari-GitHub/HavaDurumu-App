69471e87c32cec7e439561e721d1e5b303337bf5
Jafari-GitHub <masomashams1@gmail.com>
2024-09-11 13:13:16 -0700

    Added the search functionality using the API

--- src/script.js
+++ src/script.js
@@ -1,11 +1,22 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+function searchCity(city) {
+  let apiKey = "0ac05a75f3403b1d6a05e45ota81c943";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  let value = searchInput.value;
-
-  cityElement.innerHTML = value.charAt(0).toUpperCase() + value.slice(1);
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Kabul");
