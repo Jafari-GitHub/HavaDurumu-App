2f9138a6902ebefe58d84cfe306611d39ce23135
Jafari-GitHub <masomashams1@gmail.com>
2024-09-12 11:50:55 -0700

    Injected more weather data

--- src/script.js
+++ src/script.js
@@ -4,6 +4,34 @@ function refreshWeather(response) {
   temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  console.log(date);
+  timeElement.innerHTML = formatDate(date);
+}
+function formatDate(date) {
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${hour}:${minute}, ${day}`;
 }
 function searchCity(city) {
   let apiKey = "0ac05a75f3403b1d6a05e45ota81c943";
